b3e776b657c776055c84ce98b1b45a3734556b7f
Mihir K <kasaremihir2004@gmail.com>
2024-11-16 06:34:35 +0530

    backend added

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = "http://localhost:5000/api"; // Adjust based on your backend URL
+const API_BASE_URL = "https://carapp-j62n.onrender.com/api"; // Adjust based on your backend URL
 
 export default API_BASE_URL;
